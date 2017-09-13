f548a2073b78552127d89136ba5753310308a45f
Kevin Compton <kevincompton@Kevins-iMac.local>
2017-09-13 08:51:29 -0700

    update

--- resources/views/partials/_new_product.blade.php
+++ resources/views/partials/_new_product.blade.php
@@ -6,16 +6,21 @@
             <input name="user_id" type="hidden" value="{{ $user->id }}">
             <div class="row">
                 <div class="col-md-6">
-                    <div class="form-group">
+                    <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
                         <label for="name">Product Name</label>
                         <input autofocus name="name" type="text" class="form-control" id="name" placeholder="Product Name">
+                        @if ($errors->has('name'))
+                            <span class="help-block">
+                                <strong>{{ $errors->first('name') }}</strong>
+                            </span>
+                        @endif
                     </div>
                     <div class="form-group">
                         <label for="strength">CBD Strength</label>
                         <select name="strength_cbd">
                            <option>0-20 mg CBD</option>
                            <option>21-100 mg CBD</option>
-                           <option>101-250 CBD</option>
+                           <option>101-250 mg CBD</option>
                            <option>251+ mg CDB</option>
                         </select>
                     </div>
@@ -24,30 +29,45 @@
                         <select name="strength_thc">
                            <option>0-20 mg THC</option>
                            <option>21-100 mg THC</option>
-                           <option>101-250 THC</option>
+                           <option>101-250 mg THC</option>
                            <option>251+ mg THC</option>
                         </select>
                     </div>
                 </div>
                 <div class="col-md-6">
-                    <div class="form-group">
+                    <div class="form-group{{ $errors->has('description') ? ' has-error' : '' }}">
                         <label for="description">Product Description</label>
                         <textarea class="form-control" name="description" id="description" rows="3" placeholder="Product Description"></textarea>
+                        @if ($errors->has('description'))
+                            <span class="help-block">
+                                <strong>{{ $errors->first('description') }}</strong>
+                            </span>
+                        @endif
                     </div>
                 </div>
             </div>
             <div class="row">
                 <div class="col-md-6">
-                    <div class="form-group">
-                        <label for="description">Product Ingredients</label>
+                    <div class="form-group{{ $errors->has('ingredients') ? ' has-error' : '' }}">
+                        <label for="ingredients">Product Ingredients</label>
                         <textarea class="form-control" name="ingredients" id="ingredients" rows="3" placeholder="Product Ingredients"></textarea>
+                        @if ($errors->has('ingredients'))
+                            <span class="help-block">
+                                <strong>{{ $errors->first('ingredients') }}</strong>
+                            </span>
+                        @endif
                     </div>
                 </div>
                 <div class="col-md-6">
-                    <div class="form-group">
+                    <div class="form-group{{ $errors->has('image') ? ' has-error' : '' }}">
                         <label for="image">Product Image</label>
                         <input type="file" id="image" name="image">
                         <p class="help-block">Example block-level help text here.</p>
+                        @if ($errors->has('image'))
+                            <span class="help-block">
+                                <strong>{{ $errors->first('image') }}</strong>
+                            </span>
+                        @endif
                     </div>  
                 </div>
             </div>
